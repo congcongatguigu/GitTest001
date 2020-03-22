5ee425cb4ffc7b0d303ce252248a369c2cf67f9b
zhangyuanatguigu <904135144@qq.com>
2020-03-22 11:44:14 +0800

    1cc

--- src/com/atguigu/bean/Hello.java
+++ src/com/atguigu/bean/Hello.java
@@ -3,6 +3,6 @@ package com.atguigu.bean;
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("zy");
-		System.out.println("cc");
+		System.out.println("cc1");
 	}
 }
